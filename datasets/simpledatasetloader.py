38ba7546c19bd6593f495f72098851106ef9fe3f
NinhTruong1412 <ninh.truong@deliveree.com>
2023-07-21 18:47:09 +0700

    first commit

---
+++ datasets/simpledatasetloader.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+import numpy as np
+import cv2
+import os
+
+class SimpleDatasetLoader:
+    def __init__(self, preprocessors=None):
+        # store the image preprocessor
+        self.preprocessors = preprocessors
+
+        # if the preprocessors are None, initialize them as an
+        # empty list
+        if self.preprocessors is None:
+            self.preprocessors = []
